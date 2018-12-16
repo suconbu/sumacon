668b42f1562a473a3d7f0503a0f1c996561f7362
Daisuke Miwa <suconbu@gmail.com>
2018-12-16 15:54:40 +0000

    Initial commit

---
+++ FormMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Suconbu.Sumacon
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Suconbu.MobileDebugging;
+
+namespace Suconbu.Sumacon
+{
+    public partial class FormMain : Form
+    {
+        MobileDevice d;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.d = MobileDevice.Open(@"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe");
+            this.d.DeviceInfoUpdateIntervalMilliseconds = 1000;
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(this.d.LogReceiving)
+                {
+                    this.d.StopLogReceive();
+                }
+                else
+                {
+                    this.d.StartLogReceive(log =>
+                    {
+                        Trace.TraceInformation(log.Message);
+                    });
+                }
+            }
+            else if(e.KeyCode == Keys.P)
+            {
+                this.d.GetScreenCaptureAsync(image =>
+                {
+                    this.Invoke((MethodInvoker)(() => { this.pictureBox1.Image = image; }));
+                });
+            }
+        }
+    }
+}
