35d3979edc6ed995b1fdcdd1a9859a00611ebe70
miwa <suconbu@gmail.com>
2019-06-08 15:09:25 +0900

    Script:Stop/Pause/Stepボタン追加

--- FormScript.cs
+++ FormScript.cs
@@ -15,16 +15,29 @@ namespace Suconbu.Sumacon
 {
     public partial class FormScript : FormBase
     {
+        enum RunState { Ready, Running, Paused, Stepping }
+
         readonly ToolStrip uxToolStrip = new ToolStrip() { GripStyle = ToolStripGripStyle.Hidden };
-        readonly TextBox uxTextBox = new TextBox() { Dock = DockStyle.Fill, Multiline = true, HideSelection = false };
+        readonly TextBox uxScriptTextBox = new TextBox() { Dock = DockStyle.Fill, Multiline = true, HideSelection = false };
         readonly Sumacon sumacon;
+        readonly ToolStripButton uxStopButton;
+        readonly ToolStripButton uxRunButton;
+        readonly ToolStripButton uxPauseButton;
+        readonly ToolStripButton uxStepButton;
         Memezo.Interpreter interpreter = new Memezo.Interpreter();
         int currentSourceIndex;
+        RunState runState = RunState.Ready;
+        string stepTimeoutKey;
 
         public FormScript(Sumacon sumacon)
         {
             Trace.TraceInformation(Util.GetCurrentMethodName());
             InitializeComponent();
+
+            this.uxStopButton = this.uxToolStrip.Items.Add("Stop", null, (s, ee) => this.OnStop()) as ToolStripButton;
+            this.uxRunButton = this.uxToolStrip.Items.Add("Run", null, (s, ee) => this.OnRun()) as ToolStripButton;
+            this.uxPauseButton = this.uxToolStrip.Items.Add("Pause", null, (s, ee) => this.OnPause()) as ToolStripButton;
+            this.uxStepButton = this.uxToolStrip.Items.Add("Step", null, (s, ee) => this.OnStep()) as ToolStripButton;
 
             this.sumacon = sumacon;
         }
@@ -34,27 +47,19 @@ namespace Suconbu.Sumacon
             Trace.TraceInformation(Util.GetCurrentMethodName());
             base.OnLoad(e);
 
-            this.uxToolStrip.Items.Add("Run", null, (s, ee) => this.Run());
-
-            this.Controls.Add(this.uxTextBox);
+            this.Controls.Add(this.uxScriptTextBox);
             this.Controls.Add(this.uxToolStrip);
-            this.uxTextBox.Font = new Font(Properties.Resources.MonospaceFontName, this.uxTextBox.Font.Size);
+            this.uxScriptTextBox.Font = new Font(Properties.Resources.MonospaceFontName, this.uxScriptTextBox.Font.Size);
 
             this.interpreter.Install(new Memezo.StandardLibrary(), new Memezo.RandomLibrary());
             this.interpreter.ErrorOccurred += (s, errorInfo) => this.sumacon.WriteConsole(errorInfo.ToString());
-            this.interpreter.StatementReached += (s, location) =>
-            {
-                var startIndex = this.interpreter.Source.LastIndexOf(Environment.NewLine, location.CharIndex);
-                startIndex = (startIndex >= 0) ? (startIndex + Environment.NewLine.Length) : 0;
-                var endIndex = this.interpreter.Source.IndexOf(Environment.NewLine, location.CharIndex);
-                endIndex = (endIndex >= 0) ? endIndex : this.interpreter.Source.Length;
-                this.uxTextBox.SelectionStart = startIndex;
-                this.uxTextBox.SelectionLength = endIndex - startIndex;
-            };
+            this.interpreter.StatementReached += (s, location) => this.UpdateScriptSelection(location.CharIndex);
             this.interpreter.Functions["print"] = (a) => { this.sumacon.WriteConsole(a.Count >= 1 ? a.First().ToString() : null); return Memezo.Value.Zero; };
             this.interpreter.Functions["tap"] = (a) => { this.sumacon.WriteConsole(a.Count >= 3 ? $"tap x:{a[0]} y:{a[1]} duration:{a[2]}" : null); return Memezo.Value.Zero; };
 
             this.LoadSettings();
+
+            this.UpdateControlState();
         }
 
         protected override void OnClosing(CancelEventArgs e)
@@ -63,36 +68,127 @@ namespace Suconbu.Sumacon
             this.SaveSettings();
         }
 
-        void Run()
+        void OnStop()
         {
-            this.interpreter.Source = this.uxTextBox.Text;
-            //this.uxTextBox.ReadOnly = true;
-
+            this.interpreter.Source = null;
             this.currentSourceIndex = 0;
-            this.AutoStep();
+            this.uxScriptTextBox.SelectionStart = 0;
+            this.uxScriptTextBox.SelectionLength = 0;
+
+            this.runState = RunState.Ready;
+            this.UpdateControlState();
         }
 
-        void AutoStep()
+        void OnRun()
         {
-            Delay.SetTimeout(() =>
+            this.interpreter.Source = this.interpreter.Source ?? this.uxScriptTextBox.Text;
+            this.RunAuto();
+
+            this.runState = RunState.Running;
+            this.UpdateControlState();
+        }
+
+        void OnPause()
+        {
+            Delay.ClearTimeout(this.stepTimeoutKey);
+
+            this.runState = RunState.Paused;
+            this.UpdateControlState();
+        }
+
+        void OnStep()
+        {
+            this.interpreter.Source = this.interpreter.Source ?? this.uxScriptTextBox.Text;
+            this.RunOneStep();
+
+            this.runState = RunState.Stepping;
+            this.UpdateControlState();
+        }
+
+        void RunAuto()
+        {
+            this.stepTimeoutKey = Delay.SetTimeout(() =>
             {
-                var result = this.interpreter.Step(this.currentSourceIndex, out var nextIndex);
-                if (result && nextIndex >= 0)
+                if (this.runState == RunState.Running)
                 {
-                    this.currentSourceIndex = nextIndex;
-                    this.AutoStep();
+                    if (this.RunOneStep())
+                    {
+                        this.RunAuto();
+                    }
                 }
-            }, 100, this);
+            }, 100, this, this.stepTimeoutKey, true);
+        }
+
+        bool RunOneStep()
+        {
+            var result = this.interpreter.Step(this.currentSourceIndex, out var nextIndex);
+            if (result && nextIndex >= 0)
+            {
+                this.currentSourceIndex = nextIndex;
+            }
+            return result;
+        }
+
+        void UpdateScriptSelection(int index)
+        {
+            var startIndex = this.interpreter.Source.LastIndexOf(Environment.NewLine, index);
+            startIndex = (startIndex >= 0) ? (startIndex + Environment.NewLine.Length) : 0;
+
+            var endIndex = this.interpreter.Source.IndexOf(Environment.NewLine, index);
+            endIndex = (endIndex >= 0) ? endIndex : this.interpreter.Source.Length;
+
+            this.uxScriptTextBox.SelectionStart = startIndex;
+            this.uxScriptTextBox.SelectionLength = endIndex - startIndex;
         }
 
         void LoadSettings()
         {
-            this.uxTextBox.Text = Properties.Settings.Default.ScriptText;
+            this.uxScriptTextBox.Text = Properties.Settings.Default.ScriptText;
         }
 
         void SaveSettings()
         {
-            Properties.Settings.Default.ScriptText = this.uxTextBox.Text;
+            Properties.Settings.Default.ScriptText = this.uxScriptTextBox.Text;
+        }
+
+        void UpdateControlState()
+        {
+            if (this.runState == RunState.Ready)
+            {
+                this.uxStopButton.Enabled = false;
+                this.uxRunButton.Enabled = true;
+                this.uxPauseButton.Enabled = false;
+                this.uxStepButton.Enabled = true;
+                this.uxScriptTextBox.ReadOnly = false;
+            }
+            else if (this.runState == RunState.Running)
+            {
+                this.uxStopButton.Enabled = true;
+                this.uxRunButton.Enabled = false;
+                this.uxPauseButton.Enabled = true;
+                this.uxStepButton.Enabled = false;
+                this.uxScriptTextBox.ReadOnly = true;
+            }
+            else if (this.runState == RunState.Stepping)
+            {
+                this.uxStopButton.Enabled = true;
+                this.uxRunButton.Enabled = true;
+                this.uxPauseButton.Enabled = false;
+                this.uxStepButton.Enabled = true;
+                this.uxScriptTextBox.ReadOnly = true;
+            }
+            else if (this.runState == RunState.Paused)
+            {
+                this.uxStopButton.Enabled = true;
+                this.uxRunButton.Enabled = true;
+                this.uxPauseButton.Enabled = false;
+                this.uxStepButton.Enabled = true;
+                this.uxScriptTextBox.ReadOnly = true;
+            }
+            else
+            {
+                Debug.Assert(false);
+            }
         }
     }
 }
